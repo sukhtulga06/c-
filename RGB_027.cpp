dc784d330cd22dd2e2dba599571d7d84acb27814
sukhtulga06 <81095332+sukhtulga06@users.noreply.github.com>
2021-03-26 18:09:29 +0800

    Add files via upload

---
+++ RGB_027.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+//tentsuu_5
+using namespace std;
+
+int main(){
+	
+	int x,y,z,s=0;
+	cin>>x>>y>>z;
+	if(x==5){s=s+1;
+	}
+	if(y==5){s=s+1;
+	}
+	if(z==5){s=s+1;
+	}
+	cout<<s;
+}
