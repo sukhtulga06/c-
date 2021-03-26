dab222f74533353892180f59fa328b9adfc65f6d
sukhtulga06 <81095332+sukhtulga06@users.noreply.github.com>
2021-03-26 18:11:09 +0800

    Add files via upload

---
+++ RGB_032.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+//tegsh_toonuud_yes
+using namespace std;
+
+int main(){
+	int x,y,z;
+	cin>>x>>y>>z;
+	if(x%2==0){
+		cout<<"YES"<<endl;
+	}else cout<<"NO"<<endl;
+	if(y%2==0){
+		cout<<"YES"<<endl;
+	}else cout<<"NO"<<endl;
+	if(z%2==0){
+		cout<<"YES"<<endl;
+	}else cout<<"NO"<<endl;
+	return 0;
+	}
