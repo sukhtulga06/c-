7a62e5544ac700c234ef1220ad075cd74c42418b
sukhtulga06 <81095332+sukhtulga06@users.noreply.github.com>
2021-03-27 15:25:05 +0800

    Add files via upload

---
+++ RGB_044.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+//uliral
+using namespace std;
+
+int main(){
+	
+	int x;
+	cin>>x;
+	if(x>1 && x<5){
+		cout<<"Spring";
+	}else if(x>4 && x<8){
+		cout<<"Summer";
+	}else if(x>7 && x<11){
+		cout<<"Autumn";
+	}else if(x>10 && x<2){
+		cout<<"Winter";
+	}return 0;
+
