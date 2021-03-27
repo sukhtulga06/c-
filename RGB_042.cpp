a4c934729ec7020269180ce183969f58166e52cd
sukhtulga06 <81095332+sukhtulga06@users.noreply.github.com>
2021-03-27 15:24:32 +0800

    Add files via upload

---
+++ RGB_042.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+//5aas_onts_ruu
+using namespace std;
+
+int main(){
+	
+	int x;
+	cin>>x;
+	if(x==5){
+		cout<<"Onts";
+	}else if(x==4){
+		cout<<"Sain";
+	}else if(x==3){
+		cout<<"Dund";
+	}else if(x==2){
+		cout<< "Muu";
+	}
+	return 0;
+}
