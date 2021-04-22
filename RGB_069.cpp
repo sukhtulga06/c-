154a83a4a17233b32dd251da397416651ac13759
sukhtulga06 <81095332+sukhtulga06@users.noreply.github.com>
2021-04-22 17:15:53 +0800

    Add files via upload

---
+++ RGB_069.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+//davhar_davtalt_1
+using namespace std;
+
+int main(){
+	int x,i,j;
+	cin>>x;
+	if(x<10);
+	for(j=x;j>0;j--){
+		cout<<j<<" ";
+	}
+	
+	cout<<endl;
+	
+	return 0;
+}
+
