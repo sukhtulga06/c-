be1555c1b5667ae1b9f7544ce97ef027a3a451b7
sukhtulga06 <81095332+sukhtulga06@users.noreply.github.com>
2021-04-22 17:15:18 +0800

    Add files via upload

---
+++ RGB_067.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+//9oos_tsipr_hurtel
+using namespace std;
+
+int main(){
+	
+	int x,i=9;
+	cin>>x;
+	for(i==9;i>=x;i=i-0)
+	{
+	i=(i-1);
+	cout<<i+1<<endl;
+	}
+	
+	return 0;
+}
