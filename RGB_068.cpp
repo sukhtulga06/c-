0851c72243f7aa2f15e504b77cf3b11990eb3024
sukhtulga06 <81095332+sukhtulga06@users.noreply.github.com>
2021-04-22 17:15:37 +0800

    Add files via upload

---
+++ RGB_068.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+//davhar_davtalt_0
+using namespace std;
+
+int main(){
+	
+	int x,i,j;
+	cin>>x;
+	for(i=1;i<=x;i++){
+	for(j=1;j<=x;j++){
+	cout<<j;
+	}
+	cout<<endl;
+	}
+	return 0;
+}
